889e90e2d688a1953eb84c9d89f1bb656b70b983
fbmnds <fbmnds7@fn.de>
2020-12-05 15:16:18 +0100

    Scale to mm

--- views/view.lisp
+++ views/view.lisp
@@ -1,9 +1,14 @@
 
 (in-package #:paths/view)
 
+(defparameter *mm-to-px* 3.7795275591)
+(defparameter *width* 100)
+(defparameter *height* 100)
+
 (defclass window (glut:window) 
   ()
   (:default-initargs
+   :width *width* :height *height*
    :mode '(:rgba)))
 
 ;; Init
@@ -17,13 +22,18 @@
   (gl:clear :color-buffer-bit)               ; clear buffer
                                         ; draw a square
   (gl:begin :line-loop)
-  (mapc #'(lambda (c) (%gl:vertex-2d (car c) (cdr c))) *path*)
+  (mapc #'(lambda (c)
+            (%gl:vertex-2d (/ (* *mm-to-px* (car c)) *height*)
+                           (/ (* *mm-to-px* (cdr c)) *height*)))
+        *path*)
   (gl:end)
   (gl:flush))                                ; show window
 
 ;; Main
-(defun view (path)
+(defun view (path &optional width height)
   (setf *path* path)
-  (glut:display-window (make-instance 'window) path))  ; create window
+  (when width (setf *width* (floor (* *mm-to-px* 1.1 width))))
+  (when height (setf *height* (floor (* *mm-to-px* 1.1 height))))
+  (glut:display-window (make-instance 'window)))  ; create window
 
 
