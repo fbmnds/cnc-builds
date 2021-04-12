a35546f9b06754031526419aa995999c243d4569
fbmnds <fbmnds7@fn.de>
2021-04-12 14:05:23 +0200

    Update the load script

--- load.lisp
+++ load.lisp
@@ -4,9 +4,10 @@
 
 (paths/box-tests:run-tests)
 
-;;(in-package paths/emitt)
-;;(defparameter path (car paths/box-tests::tbox))
+(in-package paths/emitt)
 
-;;(cons (cons 2.5 10.5) 0.)
+(defparameter path (car paths/box-tests::tbox))
+(remove-if #'(lambda (c) (< (euklid (c- (car c) (cdr c))) 6.)) (group-2 path))
+
 
 
