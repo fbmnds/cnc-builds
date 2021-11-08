7e71c780fa58597d62a90794eb547487ddd46099
fbmnds7 <fbmnds7@fn.de>
2021-11-08 10:49:29 +0100

    Fix line breaks, use :supersede for overwriting files

--- load.lisp
+++ load.lisp
@@ -18,7 +18,7 @@
 (defparameter fn "file")
 
 (with-open-file (f "~/Desktop/test2.nc"
-                   :direction :output :if-exists :overwrite)
+                   :direction :output :if-exists :supersede)
   (format f "G21~%G91~%")
   (dolist (ln (emitt-gcode-xy-z
                (convert-path-dxyz% path tags 1. -1. 5) -0.5 1200))
@@ -42,17 +42,14 @@
 
 
 (with-open-file (f "~/projects/relays-ui/assets/data.csv"
-                   :direction :output :if-exists :overwrite)
+                   :direction :output :if-exists :supersede)
   (let ((i 0))
     (multiple-value-bind (v l-vec)
         (expand-path (car paths/box-tests::tbox)
                      '(((2.5 . 2.5) . (10.5 . 2.5))) 2 -1.5 5 2)
       (do ()
           ((= i l-vec))
-        (if (> i 0)
-            (format f "~%~3$;~3$;~3$"
+        (format f "~3$;~3$;~3$~%"
                     (aref v i) (aref v (1+ i)) (aref v (+ i 2)))
-            (format f "~3$;~3$;~3$"
-                    (aref v i) (aref v (1+ i)) (aref v (+ i 2))))
         (setf i (+ i 3))))))
 
